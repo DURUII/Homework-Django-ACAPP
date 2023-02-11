d16f851a3dbeb2c84362e5b65643561d60eaa447
durui <1511980416@qq.com>
2023-02-11 20:26:08 +0800

    first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line='<h1 style="text-align: center">my first page</h1>'
+    return HttpResponse(line)
